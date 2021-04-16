e86fc006087eabb8444812d516fef0efdf4a473f
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-16 21:13:36 +0900

    Implements : side-menu toggle

--- cafenual-front/src/layouts/Header/index.tsx
+++ cafenual-front/src/layouts/Header/index.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
+import Menu from "layouts/Menu";
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
+  const [slide, setSlide] = useState(false);
+
+  const onToggle = () => {
+    setMenu(!menu);
+    function slideMenu() {
+      return setTimeout(() => setSlide(!slide), 100)
+    }
+    slideMenu();
+  };
+
   return (
     <div id="Header">
       <div className="header-nav">
@@ -20,7 +32,14 @@ const Header = () => {
         </div>
 
         <div className="header-menu">
-          <AiOutlineMenu size="24" />
+          <div className="menu-ico" onClick={() => onToggle()}>
+            <AiOutlineMenu size="24" />
+          </div>
+          <div className={menu === false ? "" : "menu-on"}>
+            {" "}
+            {/* menu-on */}
+            <Menu menu={menu} onToggle={onToggle} slide={slide}/>
+          </div>
         </div>
       </div>
     </div>
---
+++ cafenual-front/src/layouts/Menu/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./styles.css";
+import {
+  AiOutlineClose,
+  AiOutlineStar,
+  AiOutlineUser,
+  AiOutlineDown,
+} from "react-icons/ai";
+
+interface MenuTabProps {
+  menu: boolean;
+  onToggle: any;
+  slide : boolean;
+}
+
+const Menu  : React.FunctionComponent<MenuTabProps> = ({ menu, onToggle, slide }) => {
+  return (
+    <div className="menu">
+      <div className="menu-active"></div>
+
+      <div className={ slide === false ? "menu-side" : `menu-side on`}>
+        <div className="side-tit">
+          <h1>
+            <span className="tit-logo">
+              <AiOutlineStar size="25" />
+              마이메뉴
+            </span>
+          </h1>
+          <div className="side-close" onClick={onToggle}>
+            <AiOutlineClose size="20" />
+          </div>
+        </div>
+
+        <div className="side-tab">
+          <div className="user-info">
+            <h3>
+              <span>
+                <AiOutlineUser />
+                개인정보
+              </span>
+              <span>
+                <AiOutlineDown />
+              </span>
+            </h3>
+
+            <ul className="user-menu">
+              <li>
+                <a href="">- 개인정보수정</a>
+              </li>
+              <li>
+                <a href="">- 출근·퇴근</a>
+              </li>
+              <li>
+                <a href="">- 쪽지함</a>
+              </li>
+              <li>
+                <a href="">- 체크</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
