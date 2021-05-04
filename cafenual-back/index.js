c643ca010e0315614c96fb02b6b806416a98b085
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-04 12:52:46 +0900

    Fix : require -> import 변경

--- cafenual-back/index.js
+++ cafenual-back/index.js
@@ -2,14 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import config from "./config/key";
 import User from "./models/User";
-import bodyParser from "body-parser";
+
+import usersRouter from "./api/users";
+import menusRouter from "./api/menus";
 
 const app = express();
 const PORT = 4000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-// application/json 이라는걸 클라이언트에서 받아와서 분석하기 위해 넣어준것
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 mongoose
   .connect(config.mongoURI, {
@@ -22,8 +23,8 @@ mongoose
   .catch((err) => console.log(err));
 
 // User API
-app.use("/api/users", require("./api/users"));
-app.use("/api/menus", require("./api/menus"));
+app.use("/api/users", usersRouter);
+app.use("/api/menus", menusRouter);
 
 // app.post("/api/test/test1", (req, res) => {   // 몽고디비에서 데이터 불러오기 테스트용 성공!
 //   User.find({ role: req.body.role }).exec((err, info) => {
